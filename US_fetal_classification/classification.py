d4b50eb18c29ae4028cfcbc8f4413e7e6ea28ed3
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2022-11-16 15:07:32 +0100

    updating

--- US_fetal_classification/classification.py
+++ US_fetal_classification/classification.py
@@ -13,12 +13,12 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Main for classification problem of US fetal image')
 	parser.add_argument("attribute", type=str, help="Attributo to cliification task: 'Plane' or 'Brain_plane'")
 	parser.add_argument("model_name", type=str, help="""Model name: 'MobileNetV2'; 'VGG_16'; 'DenseNet_169""")
-	
+	parser.add_argument("-frozen", default=-1, type=int, help="Number of frosez leyers in model_base")
 	args = parser.parse_args()
-
+	
 	## MODEL STRUCTURE
 	model_name = args.model_name
-	model_par = model_parameter(model_name, learning_rate=0.0001, epochs=20, frozen_layers=6)
+	model_par = model_parameter(model_name, learning_rate=0.0001, epochs=15, frozen_layers=args.frozen)
 
 	## Model dictianory of parameters
 	BACH_SIZE = model_par['BACH_SIZE']
@@ -59,19 +59,19 @@ if __name__ == '__main__':
 	## TRAINING
 	learning_rate = model_par['learning_rate']
 
-	callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5)
+	callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
 	model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
               	   loss='sparse_categorical_crossentropy',
-                   metrics=['accuracy'],
-				   callbacks=[callback])
+                   metrics=['accuracy'])
 	
 	epochs = model_par['epochs']
-	history = model.fit(train_dataset, validation_data=validation_dataset, epochs=epochs)
+	history = model.fit(train_dataset, validation_data=validation_dataset, epochs=epochs, callbacks=[callback])
 
 	hist_accuracy = [0.] + history.history['accuracy']
 	hist_val_accuracy = [0.] + history.history['val_accuracy']
 	hist_loss = history.history['loss']
 	hist_val_loss = history.history['val_loss']
+	epochs_train = len(history.history['loss'])
 	
 	## SAVE MODEL and PARAMETERS FILE
 	classification_path = 'Images_classification_'+ args.attribute
@@ -95,10 +95,13 @@ if __name__ == '__main__':
 	np.save(model_folder + '/history_val_accuracy', np.array(hist_val_accuracy))
 	np.save(model_folder + '/history_loss', np.array(hist_loss))
 	np.save(model_folder + '/history_val_loss', np.array(hist_val_loss))
+	np.save(model_folder + '/epoch_train', np.array(len(history.history['loss'])))
 
 	with open(model_folder + '/' + model_name +'_summary.txt', 'w', encoding='utf-8') as file:
 		file.write(f'\n Model Name: {model_name} \n ')
 		model.summary(print_fn=lambda x: file.write(x + '\n'))
 
+		for par in model_par.keys():
+			file.write(f'\n {par}: {model_par[par]} \n ')
 
 	
